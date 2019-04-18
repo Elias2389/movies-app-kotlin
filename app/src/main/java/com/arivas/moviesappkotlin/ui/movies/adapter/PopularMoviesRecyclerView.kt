35219b491ab28541b8252ca89296e3e411653187
arivas <andreselias2303@gmail.com>
2019-04-18 17:12:15 -0300

    Added adapter for interate the list of movies and show in device display

---
+++ app/src/main/java/com/arivas/moviesappkotlin/ui/movies/adapter/PopularMoviesRecyclerView.kt
@@ -0,0 +1,38 @@
+package com.arivas.moviesappkotlin.ui.movies.adapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.arivas.moviesappkotlin.BuildConfig
+import com.arivas.moviesappkotlin.R
+import com.arivas.moviesappkotlin.common.dto.ResultsItem
+import com.facebook.drawee.view.SimpleDraweeView
+
+class PopularMoviesRecyclerView(private val results: List<ResultsItem>): RecyclerView.Adapter<PopularMoviesRecyclerView.ViewHolder>() {
+
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
+        val view = LayoutInflater.from(p0.context).inflate(
+            R.layout.card_item,
+            p0, false
+        )
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return results.size
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, i: Int) {
+        viewHolder.title.text = results.get(i).title
+        viewHolder.img.setImageURI(BuildConfig.BASE_URL_IMAGES + results.get(i).posterPath)
+        viewHolder.content.text = results.get(i).overview
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val img: SimpleDraweeView = itemView.findViewById(R.id.image)
+        val title: TextView = itemView.findViewById(R.id.title)
+        val content: TextView = itemView.findViewById(R.id.content)
+    }
+}
--- app/src/main/java/com/arivas/moviesappkotlin/ui/movies/view/MoviesActivity.kt
+++ app/src/main/java/com/arivas/moviesappkotlin/ui/movies/view/MoviesActivity.kt
@@ -2,19 +2,28 @@ package com.arivas.moviesappkotlin.ui.movies.view
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import com.arivas.moviesappkotlin.R
 import com.arivas.moviesappkotlin.common.dto.MoviesResponse
+import com.arivas.moviesappkotlin.ui.movies.adapter.PopularMoviesRecyclerView
 import com.arivas.moviesappkotlin.ui.movies.presenter.MoviesPresenter
 import com.arivas.moviesappkotlin.ui.movies.presenter.MoviesPresenterImpl
 
 class MoviesActivity : AppCompatActivity(), MoviesView {
     private var presenter: MoviesPresenter? = null
+    private var recyclerView: RecyclerView? = null
+    private var mAdapter: RecyclerView.Adapter<*>? = null
+    private var layoutManager: RecyclerView.LayoutManager? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         presenter = MoviesPresenterImpl(this)
+        recyclerView = findViewById(R.id.recycler_view)
+
         popularMovies()
     }
 
@@ -23,10 +32,14 @@ class MoviesActivity : AppCompatActivity(), MoviesView {
     }
 
     override fun successPopularMovies(movies: MoviesResponse) {
+        layoutManager = LinearLayoutManager(this)
+        recyclerView?.layoutManager = layoutManager
 
+        mAdapter = PopularMoviesRecyclerView(movies.results!!)
+        recyclerView?.adapter = mAdapter
     }
 
     override fun error() {
-
+        
     }
 }
