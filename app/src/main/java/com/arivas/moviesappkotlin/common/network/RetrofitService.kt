35afae468ee47da5872124d71fad13a6b1dff75e
arivas <andreselias2303@gmail.com>
2019-04-18 11:53:59 -0300

    Added retrofit client and build variants

--- app/src/main/java/com/arivas/moviesappkotlin/common/network/RetrofitService.kt
+++ app/src/main/java/com/arivas/moviesappkotlin/common/network/RetrofitService.kt
@@ -1,5 +1,14 @@
 package com.arivas.moviesappkotlin.common.network
 
-object RetrofitService {
+import com.arivas.moviesappkotlin.BuildConfig
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
+object RetrofitService {
+    fun client(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
 }
