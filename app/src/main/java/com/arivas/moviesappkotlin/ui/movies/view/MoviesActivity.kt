ad1f679063b60fc360d7e7d5506819bbd00003f1
arivas <andreselias2303@gmail.com>
2019-04-20 19:37:31 -0300

    Added shimmer layout while the view charge the movies info

--- app/src/main/java/com/arivas/moviesappkotlin/ui/movies/view/MoviesActivity.kt
+++ app/src/main/java/com/arivas/moviesappkotlin/ui/movies/view/MoviesActivity.kt
@@ -4,12 +4,15 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.widget.LinearLayout
 import com.arivas.moviesappkotlin.R
 import com.arivas.moviesappkotlin.common.dto.MoviesResponse
 import com.arivas.moviesappkotlin.common.network.RetrofitService
 import com.arivas.moviesappkotlin.ui.movies.adapter.PopularMoviesRecyclerView
 import com.arivas.moviesappkotlin.ui.movies.presenter.MoviesPresenter
 import com.arivas.moviesappkotlin.ui.movies.presenter.MoviesPresenterImpl
+import io.supercharge.shimmerlayout.ShimmerLayout
 import org.koin.android.ext.android.inject
 
 
@@ -18,23 +21,30 @@ class MoviesActivity : AppCompatActivity(), MoviesView {
     private var recyclerView: RecyclerView? = null
     private var mAdapter: RecyclerView.Adapter<*>? = null
     private var layoutManager: RecyclerView.LayoutManager? = null
+    private var shimmerLayout: ShimmerLayout? = null
+    private var container: LinearLayout? = null
     private val service: RetrofitService by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        presenter = MoviesPresenterImpl(this, service)
+        shimmerLayout = findViewById(R.id.shimmer)
+        container = findViewById(R.id.container_info)
         recyclerView = findViewById(R.id.recycler_view)
+        presenter = MoviesPresenterImpl(this, service)
 
         popularMovies()
     }
 
     override fun popularMovies() {
         presenter?.popularMovies()
+        showShimmer()
     }
 
     override fun successPopularMovies(movies: MoviesResponse) {
+        hideShimmer()
+
         layoutManager = LinearLayoutManager(this)
         recyclerView?.layoutManager = layoutManager
 
@@ -44,5 +54,17 @@ class MoviesActivity : AppCompatActivity(), MoviesView {
 
     override fun error() {
 
+    }
+
+    private fun showShimmer() {
+        shimmerLayout?.startShimmerAnimation()
+        shimmerLayout?.visibility = View.VISIBLE
+        recyclerView?.visibility = View.GONE
+    }
+
+    private fun hideShimmer() {
+        recyclerView?.visibility = View.VISIBLE
+        shimmerLayout?.visibility = View.GONE
+        shimmerLayout?.stopShimmerAnimation()
     }
 }
